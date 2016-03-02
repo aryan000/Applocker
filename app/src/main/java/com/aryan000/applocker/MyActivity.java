2b4bf0fd8e7ebf1b5db53c1c2cd1f19b957a3ff3
Aryan Raj <aryan.raj000@outlook.com>
2016-03-02 23:50:53 +0530

    working and layout changing on send button

--- app/src/main/java/com/aryan000/applocker/MyActivity.java
+++ app/src/main/java/com/aryan000/applocker/MyActivity.java
@@ -1,5 +1,6 @@
 package com.aryan000.applocker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.aryan000.Applocker.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,7 @@ public class MyActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,6 +40,16 @@ public class MyActivity extends AppCompatActivity {
         return true;
     }
 
+    /* called when the user clicks the send button */
+    public  void  sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
